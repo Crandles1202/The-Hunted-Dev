7bc13f8f77dceab8165f6b6b0f6857039f6b1e99
ben <dfitz@me.com>
2025-05-09 12:36:30 -0400

    Update player_manager.lua

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -48,8 +48,8 @@ onlineCharactersPerAccount = 4 --How many characters are allowed online from a s
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
-performanceBuff = 4000
-medicalBuff = 4000
+performanceBuff = 2500
+medicalBuff = 2500
 performanceDuration = 10800 -- in seconds
 medicalDuration = 10800 -- in seconds
 
