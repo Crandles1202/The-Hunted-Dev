d190fecf038faafdc8e7031bb129ac8fc047ae8f
ben <dfitz@me.com>
2025-05-09 20:12:45 -0400

    Update planet_manager.lua

--- MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
+++ MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
@@ -41,12 +41,12 @@
 --which carries forward this exception.
 
 --Time, in seconds, that a shuttle is away for
-shuttleportAwayTime = 300
-starportAwayTime = 60
+shuttleportAwayTime = 5
+starportAwayTime = 5
 
 --Time, in seconds, that a shuttle takes to land
-shuttleportLandingTime = 11
-starportLandingTime = 14
+shuttleportLandingTime = 10
+starportLandingTime = 10
 
 --Time, in seconds, that a shuttle is landed for
 shuttleportLandedTime = 120
