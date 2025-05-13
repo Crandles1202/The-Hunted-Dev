be933b0fc8203a92a7119c260ff0636fd116d81d
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-13 15:50:57 -0400

    Update saber2hPhantom.lua
    Made Damage area fix

--- MMOCoreORB/bin/scripts/commands/saber2hPhantom.lua
+++ MMOCoreORB/bin/scripts/commands/saber2hPhantom.lua
@@ -50,9 +50,9 @@ Saber2hPhantomCommand = {
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
-	coneAngle = 360,
-	coneAction = true,
-	coneRange = 24,
+	--coneAngle = 360,
+	areaAction = true,
+	areaRange = 24,
 	forceCostMultiplier = 2.5,
 	visMod = 25,
 
