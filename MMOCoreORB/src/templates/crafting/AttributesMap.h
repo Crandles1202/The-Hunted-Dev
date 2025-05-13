74591d097e099c57578cd7473d5075d134bc46be
Chuck Randles <rac35737@gmail.com>
2025-05-12 21:26:00 -0500

    add the vectorMap for the subclass of the attribute map

--- MMOCoreORB/src/templates/crafting/AttributesMap.h
+++ MMOCoreORB/src/templates/crafting/AttributesMap.h
@@ -10,7 +10,7 @@
 
 //#define DEBUG_ATTRIBUTES_MAP
 
-class AttributesMap : public VectorMap<String, Reference<Subclasses*> > {
+class AttributesMap : public Object, public Logger, public VectorMap<String, Reference<Subclasses*> > {
 	Mutex mutex;
 
 	Vector<String> attributes;
