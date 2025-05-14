ae1c20f86768b2616fd8f38d9ffb7831ee9a83a5
Chuck Randles <rac35737@gmail.com>
2025-05-13 21:13:54 -0500

    remove and add logging to identify where attribute values are being set

--- MMOCoreORB/src/templates/crafting/AtributesMapImplementation.cpp
+++ MMOCoreORB/src/templates/crafting/AtributesMapImplementation.cpp
@@ -151,6 +151,39 @@ void AttributesMap::setCurrentValue(const String& attribute, const float value, 
 	if (values == nullptr)
 		return;
 
+		// 	//SET THE PROTECTION FOR LOOTED ITEMS
+		// //The attributes below correspond the the special protection values
+		// //armor effectivness holds the value for all non special protection values
+		// if (attributeName == "armor_effectiveness" || attributeName == "blasteffectiveness" || attributeName == "heateffectiveness" ||
+		// 	attributeName == "kineticeffectiveness" || attributeName == "energyeffectiveness" || attributeName == "electricaleffectiveness" ||
+		// 	attributeName == "coldeffectiveness" || attributeName == "acideffectiveness")
+		// {
+		// 	if (level >= 300)
+		// 	{
+		// 		float generateRandomNumber = (float) System::random(65);
+		// 		newValue = generateRandomNumber;
+		// 	}
+		// 	else if (level >= 85 && level < 300)
+		// 	{
+		// 		float generateRandomNumber = (float) System::random(55);
+		// 		newValue = generateRandomNumber;
+		// 	}
+		// 	else if (level >= 1 && level < 85)
+		// 	{
+		// 		float generateRandomNumber = (float) System::random(35);
+		// 		newValue = generateRandomNumber;
+		// 	}
+		// }
+
+		// //There is no need for this if we are setting the percintages above
+		// //This only needs to be done to looted items
+		// if (attributeName == "armor_special_effectiveness" && looted == true)
+		// {
+		// 	newValue = 0;
+		// }
+
+	info(true) << "Values of " << value << " : " << values; 	
+	info(true) << "Attribue: " << attribute << " value: " << value << " min: " << min << " max: " << max;
 	values->setValue(value);
 	values->setMaxValue(max);
 	values->setMinValue(min);
