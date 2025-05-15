0e5bb3ed7d261161ebff166152c963c03e507128
Chuck Randles <rac35737@gmail.com>
2025-05-15 11:38:18 -0500

    change log

--- MMOCoreORB/src/server/zone/objects/creature/commands/ServerLootCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ServerLootCommand.h
@@ -578,8 +578,8 @@ public:
 				int lootChance = collectionEntry->getLootChance();
 				//NOTES The-Hunted
 				//Log to see if this is where loot chance is rolled for all mobs.
-				info(true) << " SERVER LOOT COMMANDS:: Loot Chance: " << lootchance;
-
+				//info(true) << " SERVER LOOT COMMANDS:: Loot Chance: " << lootchance;
+				cout << " SERVER LOOT COMMANDS:: Loot Chance: " << lootchance << " " << endl;
 				if (lootChance <= 0)
 					continue;
 
