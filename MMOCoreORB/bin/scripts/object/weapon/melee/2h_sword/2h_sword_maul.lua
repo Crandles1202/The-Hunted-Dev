a7a36a6600558ce249a685ace6e9b59a95999390
Chuck Randles <rac35737@gmail.com>
2025-05-15 00:46:25 -0500

    forgot to save

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
@@ -70,10 +70,10 @@ object_weapon_melee_2h_sword_2h_sword_maul = object_weapon_melee_2h_sword_shared
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = BLAST,
+	damageType = LIGHTSABER,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = MEDIUM,
+	armorPiercing = HEAVY,
 
 	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
 	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
